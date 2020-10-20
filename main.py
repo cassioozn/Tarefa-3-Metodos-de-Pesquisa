0c73068ca2a1ad8cbf7d6a3af3f9394b52a0a373
Cássio Coelho <cassioozn@gmail.com>
2020-10-20 18:39:27 -0300

    Quase pronto 2

--- main.py
+++ main.py
@@ -12,13 +12,56 @@ def main():
 	print("---------------------------------------------------------------------------------------------")
 
 	with open("Nomes.txt") as base_nomes:
+		for nome in base_nomes.readlines():
+			with open("/1000/crescente_1000.csv") as file:
+				data = list(DictReader(file))
+				pesquisa_sequencial(data, nome)
+				print("---------------------------------------------------------------------------------------------")
 
-		for(nome in base_nomes)):
+		for nome in base_nomes.readlines():
+			with open("/2000/crescente_2000.csv") as file:
+				data = list(DictReader(file))
+				pesquisa_sequencial(data, nome)
+				print("---------------------------------------------------------------------------------------------")
+		
+		for nome in base_nomes.readlines():
+			with open("/3000/crescente_3000.csv") as file:
+				data = list(DictReader(file))
+				pesquisa_sequencial(data, nome)
+				print("---------------------------------------------------------------------------------------------")
+		
+		for nome in base_nomes.readlines():
+			with open("/5000/crescente_5000.csv") as file:
+				data = list(DictReader(file))
+				pesquisa_sequencial(data, nome)
+				print("---------------------------------------------------------------------------------------------")
 
-		with open("/1000/crescente_1000.csv") as file:
-			data = list(DictReader(file))
-			pesquisa_sequencial(data, "Abbey")
+		print("PESQUISA BINÁRIA")
+		print("---------------------------------------------------------------------------------------------")
 
+		for nome in base_nomes.readlines():
+			with open("/1000/crescente_1000.csv") as file:
+				data = list(DictReader(file))
+				pesquisa_binaria(data, nome)
+				print("---------------------------------------------------------------------------------------------")
+
+		for nome in base_nomes.readlines():
+			with open("/2000/crescente_2000.csv") as file:
+				data = list(DictReader(file))
+				pesquisa_binaria(data, nome)
+				print("---------------------------------------------------------------------------------------------")
+
+		for nome in base_nomes.readlines():
+			with open("/3000/crescente_3000.csv") as file:
+				data = list(DictReader(file))
+				pesquisa_binaria(data, nome)
+				print("---------------------------------------------------------------------------------------------")
+
+		for nome in base_nomes.readlines():
+			with open("/5000/crescente_5000.csv") as file:
+				data = list(DictReader(file))
+				pesquisa_binaria(data, nome)
+				print("---------------------------------------------------------------------------------------------")
 
 
 if __name__ == "__main__":
