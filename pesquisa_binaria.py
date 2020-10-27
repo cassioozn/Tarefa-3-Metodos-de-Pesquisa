f4c99a6c4413dc98443d3fcf09234eb1e4f8cf3a
Cássio Coelho <cassioozn@gmail.com>
2020-10-27 12:12:32 -0300

    Quase pronto 5

--- pesquisa_binaria.py
+++ pesquisa_binaria.py
@@ -2,17 +2,20 @@ def pesquisa_binaria(lista, nome, inicio = 0, fim = None, compara = 0):
     if fim is None:
         fim = len(lista) - 1
 
-    auxiliar = lista[int((inicio + fim)/2)]["first_name"]
+    meio = (inicio + fim)/2
+    meio = int(meio)
+
+    auxiliar = lista[meio]["first_name"]
 
     compara += 1
 
-    if(inicio > fim):
+    if inicio > fim:
         print("Nome não encontrado.")
     elif(nome == auxiliar):
         print("Nome encontrado com ." + str(compara) + " comparações.\n")
-        print(lista[int((inicio + fim)/2)])
+        print(lista[meio])
     else:
-        if(nome > auxiliar):
-            pesquisa_binaria(lista, nome, int((inicio + fim) / 2), fim, compara)
+        if nome > auxiliar:
+            pesquisa_binaria(lista, nome, meio, fim, compara)
         else:
-            pesquisa_binaria(lista, nome, inicio, int((inicio + fim)/2), compara)
+            pesquisa_binaria(lista, nome, inicio, meio, compara)
--- pesquisa_sequencial.py
+++ pesquisa_sequencial.py
@@ -2,9 +2,8 @@ def pesquisa_sequencial(lista, nome):
     tamanho = len(lista)
     compara = 0
     for i in range(0, tamanho):
-        auxiliar = lista[i]["first_name"]
         compara += 1
-        if(nome == auxiliar):
+        if(nome == lista[i]["first_name"]):
             print("Nome encontrado com ." + str(compara) + " comparações")
             print(lista[i])
             break
